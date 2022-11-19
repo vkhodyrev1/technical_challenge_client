d9dac7ab1da19acdaab3ca9a37bc71223ec2960f
VKhodyrev <vkhodyrev1@gmail.com>
2022-11-19 13:18:15 +0200

    react app created, main components functioning

---
+++ src/components/PhoneDetails.js
@@ -0,0 +1,19 @@
+import { useState, useContext, useEffect } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+
+function PhoneDetails() {
+
+    return (
+      <div className="phones">
+        
+          <header>
+            <h1>Phone Details</h1>
+          </header>
+          
+      </div>
+  
+    );
+  }
+  
+  export default PhoneDetails;
---
+++ src/components/Phones.js
@@ -0,0 +1,19 @@
+import { useState, useContext, useEffect } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+
+function Phones() {
+
+    return (
+      <div className="phones">
+        
+          <header>
+            <h1>Phones</h1>
+          </header>
+          
+      </div>
+  
+    );
+  }
+  
+  export default Phones;
